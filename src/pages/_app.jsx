cb24a37930108d777396c1491f40ccecf6615aeb
MariaEduardaAcyole <dudaacyole12@gmail.com>
2025-11-20 18:39:34 -0300

    Corrigindo imports do _app.jsx e erros de build

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,7 @@
+// src/pages/_app.jsx
 import "@/styles/globals.css";
 import Header from "@/components/Header";
-import { ThemeProvider } from "@/context/ThemeContext";
+import { ThemeProvider } from "../context/ThemeContext";
 
 export default function App({ Component, pageProps }) {
   return (
